3d40b27f43b7d0058ff8404dc852e9363293f798
julia <jerlo180908@gmail.com>
2026-03-30 16:03:06 -0300

    Adiciona documentação Swagger no server

--- src/server.js
+++ src/server.js
@@ -5,11 +5,22 @@ import quartoRoutes from './routes/quartoRoute.js';
 import pdfRoutes from './routes/pdfRoute.js';
 import fotoRoutes from './routes/fotoRoute.js';
 import autenticar from './utils/apiKey.js';
+import docApiSwagger from 'express-jsdoc-swagger';
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+
+docApiSwagger(app)({
+    info: {
+        title: 'Documentação API de Serviço de Hotéis',
+        version: '1.0.0',
+        description: 'Documentação da API de serviços de hotéis Swagger'
+    },
+    baseDir: import.meta.dirname,
+    filesPattern: './**/*.js',
+});
 
 app.get('/', (req, res) => {
     res.send('🚀 API funcionando');
